ad4b83614af68683b197112a989e22fc3cba1291
Evanson Barasa <evanzo85@gmail.com>
2020-05-20 15:53:36 +0300

    add logic to subtract, multuply and divide

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
+/*addition*/
 var add = function(number1, number2){
   return number1 + number2;
 };
+/*subtraction*/
+var subtraction = function(number1, number2){
+  return number1 - number2;
+};
+/*multiplication*/
+var multiplication = function(number1, number2){
+  return number1 * number2;
+};
+/*division*/
+var division = function(number1, number2){
+  return number1 / number2;
+};
 
 var number1= parseInt(prompt("Enter the first number:"));
-alert(number1);
+var number2= parseInt(prompt("Enter the second number:"));
+var result = division(number1,number2);
+alert(result);
